ea4c32f1f35d64ed76d948e8b3e80da7b8159c45
musa085 <150536304+musa085@users.noreply.github.com>
2025-04-26 15:48:02 +0400

    main.py

--- Task/main.py
+++ Task/main.py
@@ -1,12 +1,9 @@
-# main.py
 
-# calculator.py faylından funksiyaları import edirik
 from calculator import add, subtract
 
-# Funksiyaları siyahıda göstərəcəyik
+
 functions = [add, subtract]
 
-# Funksiyaların siyahısını çap edirik
 print("Available functions:")
 for func in functions:
-    print(func.__name__)  # funksiyanın adını çap edirik
+    print(func.__name__)  
